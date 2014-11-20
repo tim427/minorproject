160fad89b94e4c5ad6c3200c2f6493ee562663d3
Tim de Boer <tim@tim427.net>
2014-11-20 16:45:49 +0100

    Updated CollisionController

--- Unity_Projects/BasisLevelBeta/Assets/scripts/ColliderController.cs
+++ Unity_Projects/BasisLevelBeta/Assets/scripts/ColliderController.cs
@@ -4,30 +4,25 @@ using System.Collections;
 
 public class ColliderController : MonoBehaviour {
 
-	//GameObject you might collide with 
-	public Collider Schakelaar;
-	public Collider Deur;
-
+	public bool SwitchOn = false;
 
 	// Place here all "enter" events like pop-up messages
 	void OnTriggerEnter(Collider collision)
 	{
-		if (collision.collider == Schakelaar)
+		if (collision.gameObject.tag == "Schakelaar")
 		{
-
-			print ("This is a Power Switch. Do you want to toggle the switch? Y/N");
+			print ("This is a Power Switch. To toggle the switch press the spacebar");
 		}
 
-		if (collision.collider == Deur)
+		if (collision.gameObject.tag == "Deur")
 		{
 
-			print ("Enter Deur :)");
+			print ("To open the door press the spacebar");
 
 		}
 
 		if (collision.gameObject.tag == "Koelkast") 
 		{
-			print ("Koelkast enter");
 			collision.animation.Play ("Cylinder|CylinderAction");
 		}
 
@@ -36,16 +31,16 @@ public class ColliderController : MonoBehaviour {
 	// Place here al "exit" events like removing the pop-up messages
 	void OnTriggerExit(Collider collision)
 	{
-		if (collision.collider == Schakelaar)
+		if (collision.gameObject.tag == "Schakelaar")
 		{
-			print ("Exit Schakelaar :)");
+			print ("SWITCH Helper text remove");
 
 		}
 
-		if (collision.collider == Deur)
+		if (collision.gameObject.tag == "Deur")
 		{
 
-			print ("Exit Deur :)");
+			print ("DOOR Helper text remove");
 
 		}
 	}
@@ -53,17 +48,106 @@ public class ColliderController : MonoBehaviour {
 	// Place here all "stay" events, like listeners to specific buttons for further actions
 	void OnTriggerStay(Collider collision)
 	{
-		if (collision.collider == Schakelaar)
+		//yield new WaitForFixedUpdate();
+		//MAKE A SYNC FOR UPDATE/FIXED
+		if (collision.gameObject.tag == "Schakelaar")
 		{
-			if (Input.GetKeyDown(KeyCode.Space))
-				print("You just toggled the switch");
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				SwitchOn = !SwitchOn;
+				// collision.animation.Play ("Switch|SwitchAction");
+				print ("You just toggled the switch " +  (SwitchOn?"ON":"OFF"));
+			}
 		}
 
-		if (collision.collider == Deur)
+		if (collision.gameObject.tag == "Deur")
+		{
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				if (SwitchOn) {
+					// collision.animation.Play ("Door|DoorAction");
+					print ("DOOR OPENS");
+
+				} else {
+					// collision.animation.Play ("Door|DoorAction");
+					// in reverse
+					print ("FAILED! NO POWER ON DOOR");
+				}
+			}
+		}
+	}
+}using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ColliderController : MonoBehaviour {
+
+	public bool SwitchOn = false;
+
+	// Place here all "enter" events like pop-up messages
+	void OnTriggerEnter(Collider collision)
+	{
+		if (collision.gameObject.tag == "Schakelaar")
+		{
+			print ("This is a Power Switch. To toggle the switch press the spacebar");
+		}
+
+		if (collision.gameObject.tag == "Deur")
 		{
 
-			print ("Stay Deur :)");
+			print ("To open the door press the spacebar");
 
+		}
+
+		if (collision.gameObject.tag == "Koelkast") 
+		{
+			collision.animation.Play ("Cylinder|CylinderAction");
+		}
+
+	}
+
+	// Place here al "exit" events like removing the pop-up messages
+	void OnTriggerExit(Collider collision)
+	{
+		if (collision.gameObject.tag == "Schakelaar")
+		{
+			print ("SWITCH Helper text remove");
+
+		}
+
+		if (collision.gameObject.tag == "Deur")
+		{
+
+			print ("DOOR Helper text remove");
+
+		}
+	}
+
+	// Place here all "stay" events, like listeners to specific buttons for further actions
+	void OnTriggerStay(Collider collision)
+	{
+		//yield new WaitForFixedUpdate();
+		//MAKE A SYNC FOR UPDATE/FIXED
+		if (collision.gameObject.tag == "Schakelaar")
+		{
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				SwitchOn = !SwitchOn;
+				// collision.animation.Play ("Switch|SwitchAction");
+				print ("You just toggled the switch " +  (SwitchOn?"ON":"OFF"));
+			}
+		}
+
+		if (collision.gameObject.tag == "Deur")
+		{
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				if (SwitchOn) {
+					// collision.animation.Play ("Door|DoorAction");
+					print ("DOOR OPENS");
+
+				} else {
+					// collision.animation.Play ("Door|DoorAction");
+					// in reverse
+					print ("FAILED! NO POWER ON DOOR");
+				}
+			}
 		}
 	}
 }
