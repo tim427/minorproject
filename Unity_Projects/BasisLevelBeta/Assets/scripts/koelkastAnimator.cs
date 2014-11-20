09a730e684782dffe061a53b513326cd5a8a77ea
2ArounD <arnoudjonker@gmail.com>
2014-11-20 11:40:23 +0100

    geanimeerde deuren toegevoegd
    + wat kleine veranderingen

---
+++ Unity_Projects/BasisLevelBeta/Assets/scripts/koelkastAnimator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class koelkastAnimator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Invoke ("openKoelkast", 2);
+		
+	}
+	
+	// Update is called once per frame
+	void openKoelkast () 
+	{
+		animation.Play ("Cylinder|CylinderAction");
+	}
+}
