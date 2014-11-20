2be189dff219d7d08176480c012584ce22013f57
Tim de Boer <tim@tim427.net>
2014-11-20 11:28:23 +0100

    Updated object trigger events

--- Unity_Projects/BasisLevelBeta/Assets/ColliderController.cs
+++ Unity_Projects/BasisLevelBeta/Assets/ColliderController.cs
@@ -6,98 +6,56 @@ public class ColliderController : MonoBehaviour {
 
 	//GameObject you might collide with 
 	public Collider Schakelaar;
-	public Collider DeurLinks;
-	public Collider DeurRechts;
+	public Collider Deur;
 
-	//if you'd like to call a function on yourself when you collide with Schakelaar
-	public string MessageToSelf;
 
-	//calls a function on the collider object
-	public string MessageToCollider;
-
-	//If you want to call a function on yourself when they collide.
-	public bool SendToSelf = false;
-
-	public string OnCollisionEnterMessage, OnCollisionExitMessage, OnCollisionStayMessage,
-	OnTriggerEnterMessage, OnTriggerExitMessage, OnTriggerStayMessage;
-
-	void OnCollisionEnter(Collision collision)
-	{
-		if (collision.collider == Schakelaar) {
-			Schakelaar.SendMessage (OnCollisionEnterMessage, SendMessageOptions.DontRequireReceiver);
-			if (SendToSelf == true) {
-				this.SendMessage (MessageToSelf, SendMessageOptions.DontRequireReceiver);
-			}
-		}
-	}
-
-	void OnCollisionExit(Collision collision)
-	{
-		if (collision.collider == Schakelaar)
-		{
-			Schakelaar.SendMessage(OnCollisionExitMessage, SendMessageOptions.DontRequireReceiver);
-
-			if (SendToSelf == true)
-			{
-				this.SendMessage(MessageToSelf, SendMessageOptions.DontRequireReceiver);
-			}
-
-		}
-	}
-
+	// Place here all "enter" events like pop-up messages
 	void OnTriggerEnter(Collider collision)
 	{
 		if (collision.collider == Schakelaar)
 		{
-			Schakelaar.SendMessage(OnTriggerEnterMessage, SendMessageOptions.DontRequireReceiver);
 
-			if (SendToSelf == true)
-			{
-				this.SendMessage(MessageToSelf, SendMessageOptions.DontRequireReceiver);
-			}
-
-			print ("It's te god damm Schakelaar :)");
-
+			print ("This is a Power Switch. Do you want to toggle the switch? Y/N");
 		}
 
-		if (collision.collider == DeurLinks || collision.collider == DeurRechts)
+		if (collision.collider == Deur)
 		{
-			Schakelaar.SendMessage(OnTriggerEnterMessage, SendMessageOptions.DontRequireReceiver);
 
-			if (SendToSelf == true)
-			{
-				this.SendMessage(MessageToSelf, SendMessageOptions.DontRequireReceiver);
-			}
-
-			print ("It's te god damm Deur :)");
+			print ("Enter Deur :)");
 
 		}
 	}
 
+	// Place here al "exit" events like removing the pop-up messages
 	void OnTriggerExit(Collider collision)
 	{
 		if (collision.collider == Schakelaar)
 		{
-			Schakelaar.SendMessage(OnTriggerExitMessage, SendMessageOptions.DontRequireReceiver);
+			print ("Exit Schakelaar :)");
 
-			if (SendToSelf == true)
-			{
-				this.SendMessage(MessageToSelf, SendMessageOptions.DontRequireReceiver);
-			}
+		}
+
+		if (collision.collider == Deur)
+		{
+
+			print ("Exit Deur :)");
 
 		}
 	}
 
+	// Place here all "stay" events, like listeners to specific buttons for further actions
 	void OnTriggerStay(Collider collision)
 	{
 		if (collision.collider == Schakelaar)
 		{
-			Schakelaar.SendMessage(OnTriggerStayMessage, SendMessageOptions.DontRequireReceiver);
+			if (Input.GetKeyDown(KeyCode.Space))
+				print("You just toggled the switch");
+		}
 
-			if (SendToSelf == true)
-			{
-				this.SendMessage(MessageToSelf, SendMessageOptions.DontRequireReceiver);
-			}
+		if (collision.collider == Deur)
+		{
+
+			print ("Stay Deur :)");
 
 		}
 	}
