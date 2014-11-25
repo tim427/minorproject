31f424837d6fb3c20f4b6e90468c5aec721b8504
Tim de Boer <tim@tim427.net>
2014-11-25 12:32:33 +0100

    Updated onScreenText

--- Unity_Projects/BasisLevelBeta/Assets/scripts/ColliderController.cs
+++ Unity_Projects/BasisLevelBeta/Assets/scripts/ColliderController.cs
@@ -5,20 +5,28 @@ using System.Collections;
 public class ColliderController : MonoBehaviour {
 
 	public bool SwitchOn = false;
-	public string OnScreenText = "";
+	public string OnScreenText;
+	public Font Font;
+	public bool spacePressed = false;
+
+	void FixedUpdate() {
+		if (spacePressed == false && Input.GetKeyDown(KeyCode.Space))
+		{
+			spacePressed = true;
+		}
+	}
 
 	// Place here all "enter" events like pop-up messages
 	void OnTriggerEnter(Collider collision)
 	{
 		if (collision.gameObject.tag == "Schakelaar")
 		{
-			print ("This is a Power Switch. To toggle the switch press the spacebar");
+			OnScreenText = "This is a Power Switch. To toggle the switch press the spacebar";
 		}
 
 		if (collision.gameObject.tag == "Deur")
 		{
-
-			print ("To open the door press the spacebar");
+			OnScreenText = "To open the door press the spacebar";
 
 		}
 
@@ -34,15 +42,13 @@ public class ColliderController : MonoBehaviour {
 	{
 		if (collision.gameObject.tag == "Schakelaar")
 		{
-			print ("SWITCH Helper text remove");
+			OnScreenText = "";
 
 		}
 
 		if (collision.gameObject.tag == "Deur")
 		{
-
-			print ("DOOR Helper text remove");
-
+			OnScreenText = "";
 		}
 	}
 
@@ -52,33 +58,52 @@ public class ColliderController : MonoBehaviour {
 
 		if (collision.gameObject.tag == "Schakelaar")
 		{
-			if (Input.GetKeyDown (KeyCode.Space)) {
+			if (spacePressed) {
+				spacePressed = false;
 				SwitchOn = !SwitchOn;
 				// collision.animation.Play ("Switch|SwitchAction");
-				print ("You just toggled the switch " +  (SwitchOn?"ON":"OFF"));
+				OnScreenText = "You just toggled the switch " +  (SwitchOn?"ON":"OFF");
 			}
 		}
 
 		if (collision.gameObject.tag == "Deur")
 		{
-			if (Input.GetKeyDown (KeyCode.Space)) {
+			if (spacePressed) {
+				spacePressed = false;
 				if (SwitchOn) {
 					// collision.animation.Play ("Door|DoorAction");
-					print ("DOOR OPENS");
+					OnScreenText = "DOOR OPENS";
 
 				} else {
 					// collision.animation.Play ("Door|DoorAction");
 					// in reverse
-					print ("FAILED! NO POWER ON DOOR");
-					OnScreenText = "FAILED! NO POWER ON DOOR";
+					OnScreenText = "FAILED! NO POWER ON DOOR\nTWEEDE REGEL";
 				}
 			}
 		}
 	}
 
 	void OnGUI() {
+		GUI.skin.font = Font;
 
-		GUI.Button (new Rect (10,10,150,20), OnScreenText);
+		if (OnScreenText.Length > 0)
+		{
+			string[] lines = OnScreenText.Split('\n');
+			int longestLineLength = 0;
+			for (int i = 0; i < lines.Length; i++)
+			{
+				if (lines[i].Length > longestLineLength)
+				{
+					longestLineLength = lines[i].Length;
+				}
+				i++;
+			}
+			int width = longestLineLength * 9 + 30;
+			int height = 30*lines.Length;
+			int x = Screen.width/2 - width/2;
+			int y = Screen.height/2;
+			GUI.Box(new Rect(x, y, width, height), OnScreenText);
+		}
 
 	}
 		
