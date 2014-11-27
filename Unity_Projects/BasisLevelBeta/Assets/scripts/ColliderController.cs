b30497e8bc3b1c046e12f3f2150fd50f4131fd91
Tim de Boer <tim@tim427.net>
2014-11-27 10:30:26 +0100

    Killed the ForceUpdate bug

--- Unity_Projects/BasisLevelBeta/Assets/scripts/ColliderController.cs
+++ Unity_Projects/BasisLevelBeta/Assets/scripts/ColliderController.cs
@@ -4,28 +4,62 @@ using System.Collections;
 
 public class ColliderController : MonoBehaviour {
 
+	private Collider Collider = null;
 	public bool switchOn = false;
     public bool hasCollectableTorch = false;
     public bool showCollectables = false;
 	public string onScreenText;
 	public Font Font;
-	public bool spacePressed = false;
 
-	void FixedUpdate() {
-		if (spacePressed == false && Input.GetKeyDown(KeyCode.Space))
-		{
-			spacePressed = true;
-		}
+	void Update() {
 
-        if (Input.GetKeyDown(KeyCode.Tab))
+		if (Input.GetKeyDown(KeyCode.Tab))
         {
             showCollectables = !showCollectables;
         }
+
+		if (Collider != null && Collider.gameObject.tag == "Switch")
+		{
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				switchOn = !switchOn;
+				// collision.animation.Play ("Switch|SwitchAction");
+				onScreenText = "You just toggled the switch " +  (switchOn?"ON":"OFF");
+				foreach (GameObject ceilingLight in GameObject.FindGameObjectsWithTag("CeilingLights")) {
+					ceilingLight.light.color = (switchOn?new Color(0.64F, 0.82F, 1F, 1F):new Color(1F, 0F, 0F, 1F));
+				}
+				GameObject.FindGameObjectWithTag("MainCamera").camera.backgroundColor = (switchOn?new Color(0.64F, 0.82F, 1F, 1F):new Color(0.95F, 0.25F, 0.25F, 1F));
+			}
+		}
+		
+		if (Collider != null && Collider.gameObject.tag == "Door")
+		{
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				if (switchOn) {
+					// collision.animation.Play ("Door|DoorAction");
+					onScreenText = "DOOR OPENS";
+					
+				} else {
+					// collision.animation.Play ("Door|DoorAction");
+					// in reverse
+					onScreenText = "FAILED! NO POWER ON DOOR";
+				}
+			}
+		}
+		
+		if (Collider != null && Collider.gameObject.tag == "Torch")
+		{
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				onScreenText = "Successfully collected the Torch";
+				hasCollectableTorch = true;
+				Destroy(Collider.gameObject);
+			}
+		}
 	}
 
 	// Place here all "enter" events like pop-up messages
 	void OnTriggerEnter(Collider collision)
 	{
+		Collider = collision;
 		if (collision.gameObject.tag == "Switch")
 		{
 			onScreenText = "This is a Power Switch. To toggle the switch press the spacebar";
@@ -43,59 +77,15 @@ public class ColliderController : MonoBehaviour {
 
         if (collision.gameObject.tag == "Torch") 
         {
-            onScreenText = "To collect the Torch press the spacebar!";
+			onScreenText = "To collect the Torch press the spacebar!";
         }
 	}
 
 	// Place here al "exit" events like removing the pop-up messages
 	void OnTriggerExit(Collider collision)
 	{
-        onScreenText = "";
-	}
-
-	// Place here all "stay" events, like listeners to specific buttons for further actions
-	void OnTriggerStay(Collider collision)
-	{
-
-		if (collision.gameObject.tag == "Switch")
-		{
-			if (spacePressed) {
-				spacePressed = false;
-				switchOn = !switchOn;
-				// collision.animation.Play ("Switch|SwitchAction");
-				onScreenText = "You just toggled the switch " +  (switchOn?"ON":"OFF");
-                foreach (GameObject ceilingLight in GameObject.FindGameObjectsWithTag("CeilingLights")) {
-                    ceilingLight.light.color = (switchOn?new Color(0.64F, 0.82F, 1F, 1F):new Color(1F, 0F, 0F, 1F));
-                }
-                GameObject.FindGameObjectWithTag("MainCamera").camera.backgroundColor = (switchOn?new Color(0.64F, 0.82F, 1F, 1F):new Color(0.95F, 0.25F, 0.25F, 1F));
-			}
-		}
-
-		if (collision.gameObject.tag == "Door")
-		{
-			if (spacePressed) {
-				spacePressed = false;
-				if (switchOn) {
-					// collision.animation.Play ("Door|DoorAction");
-				    onScreenText = "DOOR OPENS";
-
-				} else {
-					// collision.animation.Play ("Door|DoorAction");
-					// in reverse
-					onScreenText = "FAILED! NO POWER ON DOOR";
-				}
-			}
-		}
-
-        if (collision.gameObject.tag == "Torch")
-        {
-            if (spacePressed) {
-                spacePressed = false;
-                onScreenText = "Successfully collected the Torch";
-                hasCollectableTorch = true;
-                Destroy(collision.gameObject);
-            }
-        }
+		Collider = null;
+		onScreenText = "";
 	}
 
 	void OnGUI() {
