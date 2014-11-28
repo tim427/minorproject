0b0084d5b860e1005ed969abd6f122467b7958e6
XanderdeRonde <xanderderonde@gmail.com>
2014-11-28 19:32:28 +0100

    Options menu script
    Script for the options menu

--- Unity_Projects/BasisLevelBeta/Assets/settings.cs
+++ Unity_Projects/BasisLevelBeta/Assets/settings.cs
@@ -5,111 +5,205 @@ public class settings : MonoBehaviour {
 	private bool menu = true;
 	private bool options = false;
 	private bool video = false;
-	private bool audio = false;
+	private bool audiosettings = false;
 	
 	public float fieldOfView = 60f;
 	public float sfxVolume = 0.5f;
 	public float musicVolume = 0.5f;
 	
+	public int resX;
+	public int resY;
+
+	
 	void OnGUI() {
 		if (menu) {
-			//Start the game, view/edit Options or quit the game
-			if (GUI.Button(new Rect (Screen.width / 2, Screen.height / 2 - 50, 100, 30), "Start game")) {
-				//Start the game
-			}
-			
-			if (GUI.Button(new Rect (Screen.width / 2, Screen.height / 2, 100, 30), "Options")) {
-				menu = false;
-				options = true;
-			}
-			
-			if (GUI.Button(new Rect (Screen.width / 2, Screen.height / 2 + 50, 100, 30), "Quit")) {
-				Application.Quit ();
-			}
+			mainMenu();
 		}
 		
 		if (options) {
-			//View/edit video settings, view/edit audio settings or go back to the menu
-			if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 - 50, 100, 30), "Video Settings")) {
-				options = false;
-				video = true;
-			}
-			
-			if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 - 0, 100, 30), "Audio Settings")) {
-				options = false;
-				audio = true;
-			}
-			
-			if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 + 50, 100, 30), "Back")) {
-				menu = true;
-				options = false;
-			}
+			optionsMenu();
 		}
 		
 		if (video) {
-			//Edit video settings (MSAA slider (0/2/4/8), Resolution(1080p/720p/480p), graphics quality,
-			//field of view (60-110), Anisotropic filtering (on/off), vSync (on/off)
-			string[] qualities = QualitySettings.names;
-
-			//for loop to create Quality buttons
-			for (int i=0; i < qualities.Length; i++) {
-				if(GUI.Button(new Rect(Screen.width/2 - 100, Screen.height/2 - 120 + i * 30, 100, 30), qualities[i])){
-					QualitySettings.SetQualityLevel (i, true);
-				}
-			}
-			GUI.Label(new Rect (Screen.width / 2 - 75, Screen.height/2 - 150, 100, 30), "Quality:");
-
-			//Button to return to the previous menu
-			if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 + 90, 100, 30), "Back")) {
-				//Go back to the menu
-				video = false;
-				options = true;
-			}
-
-			//Slider to adjust the field of view
-			float tempFoV = GUI.HorizontalSlider(new Rect(Screen.width/2, Screen.height/2, 100, 30), fieldOfView, 60f, 110f);
-			fieldOfView = (int)tempFoV;
-			GUI.Label(new Rect (Screen.width / 2, Screen.height/2 - 15, 100, 30), "Field of view: " + fieldOfView);
-
-
-			if (QualitySettings.vSyncCount == 1) {
-				if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 - 120, 100, 30), "vSync: on")) {
-					QualitySettings.vSyncCount = 0;
-				}
-			}
-			else {
-				if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 - 120, 100, 30), "vSync: off")) {
-					QualitySettings.vSyncCount = 1;
-				}
-			}
-			if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 - 90, 100, 30), "Turn Anisotropic Filtering on")) {
-				QualitySettings.anisotropicFiltering = AnisotropicFiltering.ForceEnable;
-			}
-			if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 - 60, 100, 30), "Turn Anisotropic Filtering off")) {
-				QualitySettings.anisotropicFiltering = AnisotropicFiltering.Disable;
-			}
-
-		
-
+			videoOptions();
 		}
-		if (audio) {
-			//Slider for the Sound Effects volume
-			sfxVolume = GUI.HorizontalSlider(new Rect(Screen.width/2, Screen.height/2 - 60, 100, 45), sfxVolume, 0.0f, 1.0f); 
-			GUI.Label(new Rect(Screen.width/2, Screen.height/2 - 40, 150, 30), "SFX; " + sfxVolume);
-
-			//Slider for the Music volume
-			musicVolume = GUI.HorizontalSlider(new Rect(Screen.width/2, Screen.height/2 - 10, 100, 45), musicVolume, 0.0f, 1.0f); 
-			GUI.Label(new Rect(Screen.width/2, Screen.height/2 + 10, 150, 30), "Music; " + musicVolume);
-
-			//Button to return to the previous menu
-			if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 + 60, 100, 30), "Back")) {
-			//Go back to the menu
-			audio = false;
-			options = true;
-			}
-
-
-
+		
+		if (audiosettings) {
+			audioOptions ();
 		}
 	}
+	
+	//Function that describes the main menu
+	private void mainMenu() {
+		//Start the game, view/edit Options or quit the game
+		if (GUI.Button(new Rect (Screen.width / 2, Screen.height / 2 - 50, 100, 30), "Start game")) {
+			//Start the game
+		}
+		
+		if (GUI.Button(new Rect (Screen.width / 2, Screen.height / 2, 100, 30), "Options")) {
+			menu = false;
+			options = true;
+		}
+		
+		if (GUI.Button(new Rect (Screen.width / 2, Screen.height / 2 + 50, 100, 30), "Quit")) {
+			Application.Quit ();
+		}
+	}
+	
+	//Function that describes the options menu
+	private void optionsMenu() {
+		if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 - 50, 100, 30), "Video Settings")) {
+			options = false;
+			video = true;
+		}
+		
+		if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 - 0, 100, 30), "Audio Settings")) {
+			options = false;
+			audiosettings = true;
+		}
+		
+		if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 + 50, 100, 30), "Back")) {
+			menu = true;
+			options = false;
+		}
+	}
+	
+	//Function that describes the graphics options menu
+	private void videoOptions() {		
+		int currentQLevel = QualitySettings.GetQualityLevel();
+		resX = Screen.width;
+		resY = Screen.height;
+		int[] antiAliasingOptions = new int[] {0,2,4,8};
+		int antiAliasingCurrent = QualitySettings.antiAliasing;
+		int aaIndex = 0;
+		bool fs = Screen.fullScreen;
+		string[] qualities = QualitySettings.names;
+		int indexRes = 0;
+		Resolution[] resolutions = Screen.resolutions;
+		
+		if (GUI.Button(new Rect(Screen.width /2 - 150, Screen.height / 2 + -120, 150, 30), "Decrease")) {
+			if (currentQLevel > 0) {
+				QualitySettings.DecreaseLevel();
+			}
+			
+		}
+		
+		GUI.Box(new Rect (Screen.width / 2 - 150, Screen.height/2 - 85, 150, 30), "Quality: " + qualities[QualitySettings.GetQualityLevel()]);
+		
+		if (GUI.Button(new Rect(Screen.width /2 - 150, Screen.height / 2 - 50, 150, 30), "Increase")) {
+			if (currentQLevel < qualities.Length-1) {
+				QualitySettings.IncreaseLevel();
+			}
+		}
+		
+		
+		//Button to return to the previous menu
+		if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 + 90, 150, 30), "Back")) {
+			video = false;
+			options = true;
+		}
+		
+		//Slider to adjust the field of view
+		float tempFoV = GUI.HorizontalSlider(new Rect(Screen.width/2 + 150, Screen.height/2 + 40, 150, 30), fieldOfView, 60f, 110f);
+		fieldOfView = (int)tempFoV;
+		GUI.Label(new Rect (Screen.width / 2 + 180, Screen.height/2 + 50, 150, 30), "Field of view: " + fieldOfView);
+		
+		//Function that determines what the current Vsync count is, and adjusts appropriately
+		if (QualitySettings.vSyncCount == 1) {
+			if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 - 120, 150, 30), "vSync: on")) {
+				QualitySettings.vSyncCount = 0;
+			}
+		}
+		else {
+			if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 - 120, 150, 30), "vSync: off")) {
+				QualitySettings.vSyncCount = 1;
+			}
+		}
+
+		//Function that checks whether anisotropic filtering is disabled, and offers the correct button
+		if (QualitySettings.anisotropicFiltering == AnisotropicFiltering.Enable || QualitySettings.anisotropicFiltering == AnisotropicFiltering.ForceEnable) {
+			if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 - 90, 150, 30), "Anisotropic Filtering: on")) {
+				QualitySettings.anisotropicFiltering = AnisotropicFiltering.Disable;
+			}
+			
+		}
+		if (QualitySettings.anisotropicFiltering == AnisotropicFiltering.Disable) {
+			if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 - 90, 150, 30), "Anisotropic Filtering: off")) {
+				QualitySettings.anisotropicFiltering = AnisotropicFiltering.ForceEnable;
+			}
+		}
+		
+		//Function that checks what the current resolution is, and allows increasing/decreasing
+		//based on the current resolution (cant go lower/higher than certain resolutions) and fullscreen/windowed
+		for (int i = 0; i < resolutions.Length; i++) {
+			if (resolutions[i].height == resY && resolutions[i].width == resX) {
+				indexRes = i;
+			}
+		}
+		if (GUI.Button(new Rect(Screen.width /2 + 150, Screen.height / 2 - 120, 150, 30), "Decrease")) {
+			if (indexRes > 2) {
+				resX = resolutions[indexRes-1].width;
+				resY = resolutions[indexRes-1].height;
+				Screen.SetResolution(resX,resY,fs);
+			}
+		}
+		GUI.Box (new Rect(Screen.width /2 + 150, Screen.height / 2 - 85, 150, 30), "Resolution: " + resX + "x" + resY);
+		if (GUI.Button(new Rect(Screen.width /2 + 150, Screen.height / 2 - 50, 150, 30), "Increase")) {
+			if (indexRes < resolutions.Length-1) {
+				resX = resolutions[indexRes+1].width;
+				resY = resolutions[indexRes+1].height;
+				Screen.SetResolution(resX,resY,fs);
+			}
+		}
+		
+		//Button that allows switching between windowed and fullscreen mode
+		if (Screen.fullScreen) {
+			if(GUI.Button(new Rect(Screen.width/2 + 150, Screen.height / 2, 150, 30), "Windowed")) {
+				Screen.SetResolution(resX,resY, false);
+			}
+		}
+		else {
+			if(GUI.Button(new Rect(Screen.width/2 + 150, Screen.height / 2	, 150, 30), "Fullscreen")) {
+				Screen.SetResolution(resX,resY, true);
+			}
+		}
+		
+		//Function that allows MSAA to be differentiated through the values 0x, 2x, 4x and 8x
+		for (int i = 0; i < 4; i++) {
+			if (antiAliasingCurrent == antiAliasingOptions[i]) {
+				aaIndex = i;
+			}
+		}
+		if (GUI.Button(new Rect(Screen.width /2 - 150, Screen.height / 2, 150, 30), "Decrease")) {
+			if (antiAliasingCurrent > antiAliasingOptions[0]) {
+				QualitySettings.antiAliasing = antiAliasingOptions[aaIndex-1];
+			}
+		}
+		
+		GUI.Box (new Rect(Screen.width /2 - 150, Screen.height / 2 + 35, 150, 30), "Anti Aliasing: MSAAx" + antiAliasingCurrent);
+		
+		if (GUI.Button(new Rect(Screen.width /2 - 150, Screen.height / 2 + 70, 150, 30), "Increase")) {
+			if (antiAliasingCurrent < antiAliasingOptions[3]) {
+				QualitySettings.antiAliasing = antiAliasingOptions[aaIndex+1];
+			}
+		}
+	}
+	
+	//Function that describes the audio options menu 
+	private void audioOptions() {
+		//Slider for the Sound Effects volume
+		sfxVolume = GUI.HorizontalSlider(new Rect(Screen.width/2, Screen.height/2 - 60, 100, 45), sfxVolume, 0.0f, 1.0f); 
+		GUI.Label(new Rect(Screen.width/2, Screen.height/2 - 40, 150, 30), "SFX; " + sfxVolume);
+		
+		//Slider for the Music volume
+		musicVolume = GUI.HorizontalSlider(new Rect(Screen.width/2, Screen.height/2 - 10, 100, 45), musicVolume, 0.0f, 1.0f); 
+		GUI.Label(new Rect(Screen.width/2, Screen.height/2 + 10, 150, 30), "Music; " + musicVolume);
+		
+		//Button to return to the previous menu
+		if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 + 60, 100, 30), "Back")) {
+			audiosettings = false;
+			options = true;
+		}
+	}
+	
 }
