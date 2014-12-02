f8492d75959519eb69aad92417d97836d7ab4f11
2ArounD <arnoudjonker@gmail.com>
2014-12-02 10:37:12 +0100

    Procedurally floor

---
+++ Unity_Projects/BasisLevelBeta/Assets/Editor/RandomPlaneCreatorInspector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(RandomPlaneCreator))]
+public class RandomPlaneCreatorInspector : Editor {
+
+	public override void OnInspectorGUI () {
+		DrawDefaultInspector();
+
+		if (GUILayout.Button("Regenerate")) {
+			RandomPlaneCreator plane = (RandomPlaneCreator)target;
+			plane.BuildMesh();
+		}
+	}
+
+}
