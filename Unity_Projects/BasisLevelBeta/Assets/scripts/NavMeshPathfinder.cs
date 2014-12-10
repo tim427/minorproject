90929406944cf1ae03c1efb9738a0e4a66543737
2ArounD <arnoudjonker@gmail.com>
2014-12-10 15:04:17 +0100

    NAVMESH implemented
    enemy uses navmesh to find player
    
    other changes:
    
    - goats are gone
    - Jordi's floor implemented
    - as always: minor bugfixes

---
+++ Unity_Projects/BasisLevelBeta/Assets/scripts/NavMeshPathfinder.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMeshPathfinder : MonoBehaviour {
+
+	private NavMeshAgent navMesh;
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		navMesh = GetComponent<NavMeshAgent>();
+		navMesh.SetDestination(target.position);
+	}
+}
