3efc876225636ee6b9948570e4effb57d9f596b4
Tim de Boer <tim@tim427.net>
2014-12-16 12:31:29 +0100

    Updated Controllers

--- Unity_Projects/BasisLevelBeta/Assets/scripts/CameraController.cs
+++ Unity_Projects/BasisLevelBeta/Assets/scripts/CameraController.cs
@@ -1,35 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
-	private float direction;
-	private bool started = false;
-	public int maxLeft = 270;
-	public int maxRight = 90;
-	public float rotationSpeed = 1f;
-	public float angleWidth = 30f;
-	public float detectionDistance = 3f;
+		private float direction;
+		private bool started = false;
+		public int maxLeft = 270;
+		public int maxRight = 90;
+		public float rotationSpeed = 1f;
+		public float angleWidth = 30f;
+		public float detectionDistance = 3f;
 
-	void Update () {
-		float degrees = transform.FindChild("Body").rotation.eulerAngles.z;
-		if ((degrees >= maxRight-1 && degrees <= maxRight+1) || !started) {
-			started = true;
-			direction = rotationSpeed;
-		}
-		if (degrees >= maxLeft-1 && degrees <= maxLeft+1) {
-			direction = -rotationSpeed;
-		}
-		transform.FindChild("Body").Rotate(0, 0, direction);
-
-		GameObject target = GameObject.FindGameObjectWithTag ("Player");
-		Vector3 targetDir = target.transform.position - transform.FindChild("Body").position;
-		Vector3 forward = transform.FindChild("Body").up;
-		float angle = Vector3.Angle(forward, targetDir);
-		float distance = Vector3.Distance(forward, targetDir);
-		if (angle < angleWidth && distance < detectionDistance)
+		void Update ()
 		{
-			print ("CAMERA!!!");
+				float degrees = transform.FindChild ("Body").rotation.eulerAngles.z;
+				if ((degrees >= maxRight - 1 && degrees <= maxRight + 1) || !started) {
+						started = true;
+						direction = rotationSpeed;
+				}
+				if (degrees >= maxLeft - 1 && degrees <= maxLeft + 1) {
+						direction = -rotationSpeed;
+				}
+				transform.FindChild ("Body").Rotate (0, 0, direction);
+
+				GameObject target = GameObject.FindGameObjectWithTag ("Player");
+				Vector3 targetDir = target.transform.position - transform.FindChild ("Body").position;
+				Vector3 forward = transform.FindChild ("Body").up;
+				float angle = Vector3.Angle (forward, targetDir);
+				float distance = Vector3.Distance (forward, targetDir);
+				if (angle < angleWidth && distance < detectionDistance) {
+						print ("CAMERA!!!");
+				}
 		}
-	}
 }
--- Unity_Projects/BasisLevelBeta/Assets/scripts/ColliderController.cs
+++ Unity_Projects/BasisLevelBeta/Assets/scripts/ColliderController.cs
@@ -2,178 +2,184 @@
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
-
-public class ColliderController : MonoBehaviour {
+		
+public class ColliderController : MonoBehaviour
+{
+		
+		private Collider Collider = null;
+		private List<GameObject> CollectedGameObjects = new List<GameObject> ();
+		private List<GameObject> PlayedGameObjects = new List<GameObject> ();
+		private int SwitchCounter = 0;
+		public bool switchOn = false;
+		public bool showCollectables = false;
+		public string onScreenText;
+		public Font Font;
+		public AudioClip collectSound;
+		public int position_switch;
+		
+		void Update ()
+		{
 	
-	private Collider Collider = null;
-	private List<GameObject> CollectedGameObjects = new List<GameObject>();
-	private List<GameObject> PlayedGameObjects = new List<GameObject>();
-	private int SwitchCounter = 0;
-	public bool switchOn = false;
-	public bool showCollectables = false;
-	public string onScreenText;
-	public Font Font;
-	public AudioClip collectSound;
-	public int position_switch;
-
-	void Update() {
-
-		if (Input.GetKeyDown(KeyCode.Tab))
-		{
-			showCollectables = !showCollectables;
+				if (Collider != null && Collider.gameObject.tag == "Switch") {
+						if (Input.GetKeyDown (KeyCode.Space)) {
+								switchOn = !switchOn;
+								SwitchCounter ++;
+								// collision.animation.Play ("Switch|SwitchAction");
+								Collider.audio.Play ();
+								if (SwitchCounter < 20) {
+										SetOnScreenText ("You just toggled the switch " + (switchOn ? "ON" : "OFF"));
+										foreach (GameObject ceilingLight in GameObject.FindGameObjectsWithTag("CeilingLights")) {
+												ceilingLight.light.color = (switchOn ? new Color (0.64F, 0.82F, 1F, 1F) : new Color (1F, 0F, 0F, 1F));
+										}
+										GameObject.FindGameObjectWithTag ("MainCamera").camera.backgroundColor = (switchOn ? new Color (0.64F, 0.82F, 1F, 1F) : new Color (0.95F, 0.25F, 0.25F, 1F));
+								} else {
+										SetOnScreenText ("The lights are malfunctioning due to excessive usage!");
+										GameObject.FindGameObjectWithTag ("MainCamera").camera.backgroundColor = new Color (0F, 0F, 0F, 0F);
+										foreach (GameObject ceilingLight in GameObject.FindGameObjectsWithTag("CeilingLights")) {
+												ceilingLight.light.enabled = false;
+										}
+										GameObject.FindGameObjectWithTag ("DirectionalLight").light.enabled = false;
+								}
+		
+								position_switch = 10000;
+		
+								// Switch the switch down
+								if (switchOn == false) {
+										Collider.transform.FindChild ("Switch").Rotate (0, -position_switch, 0);
+								}
+								if (switchOn == true) {
+										Collider.transform.FindChild ("Switch").Rotate (0, position_switch, 0);
+								}
+						}
+				}
+		
+				if (Collider != null && Collider.gameObject.tag == "Door") {
+						if (Input.GetKeyDown (KeyCode.Space)) {
+								if (switchOn) {
+										// collision.animation.Play ("Door|DoorAction");
+										Collider.audio.Play ();
+										SetOnScreenText ("DOOR OPENS");
+										Application.LoadLevel ("nieuwe test scene");
+		
+								} else {
+										// collision.animation.Play ("Door|DoorAction");
+										// in reverse
+										SetOnScreenText ("FAILED! NO POWER ON DOOR");
+								}
+						}
+				}
+		
+				if (Collider != null && (Collider.gameObject.tag == "CollectableConsumable" || Collider.gameObject.tag == "CollectableReusable")) {
+						if (Input.GetKeyDown (KeyCode.Space)) {
+								SetOnScreenText ("Successfully collected the " + Collider.gameObject.name);
+								AudioSource.PlayClipAtPoint (collectSound, transform.position);
+								CollectedGameObjects.Add (Collider.gameObject);
+								Collider.gameObject.SetActive (false);
+								Destroy (Collider.gameObject.collider);
+								Collider = null;
+						}
+				}
 		}
 		
-		if (Collider != null && Collider.gameObject.tag == "Switch")
+		void SetOnScreenText (string text)
 		{
-			if (Input.GetKeyDown(KeyCode.Space)) {
-				switchOn = !switchOn;
-				SwitchCounter ++;
-				// collision.animation.Play ("Switch|SwitchAction");
-				Collider.audio.Play();
-				if(SwitchCounter < 20){
-					SetOnScreenText("You just toggled the switch " +  (switchOn?"ON":"OFF"));
-					foreach (GameObject ceilingLight in GameObject.FindGameObjectsWithTag("CeilingLights")) {
-						 ceilingLight.light.color = (switchOn?new Color(0.64F, 0.82F, 1F, 1F):new Color(1F, 0F, 0F, 1F));
-					}
-					GameObject.FindGameObjectWithTag("MainCamera").camera.backgroundColor = (switchOn?new Color(0.64F, 0.82F, 1F, 1F):new Color(0.95F, 0.25F, 0.25F, 1F));
-				}
-				else{
-					SetOnScreenText("The lights are malfunctioning due to excessive usage!");
-					GameObject.FindGameObjectWithTag("MainCamera").camera.backgroundColor = new Color(0F, 0F, 0F, 0F);
-					foreach (GameObject ceilingLight in GameObject.FindGameObjectsWithTag("CeilingLights")) {
-						ceilingLight.light.enabled = false;
-					}
-					GameObject.FindGameObjectWithTag("DirectionalLight").light.enabled = false;
-				}
-
-				position_switch = 10000;
-
-				// Switch the switch down
-				if (switchOn == false){
-					Collider.transform.FindChild("Switch").Rotate(0, -position_switch, 0);
-				}
-				if (switchOn == true){
-					Collider.transform.FindChild("Switch").Rotate(0, position_switch, 0);
-				}
-			}
+				onScreenText = text;
+				StartCoroutine (AutoRemoveOnScreenText (onScreenText));
 		}
 		
-		if (Collider != null && Collider.gameObject.tag == "Door")
+		IEnumerator AutoRemoveOnScreenText (string CurrentOnScreenText)
 		{
-			if (Input.GetKeyDown(KeyCode.Space)) {
-				if (switchOn) {
-					// collision.animation.Play ("Door|DoorAction");
-					Collider.audio.Play();
-					SetOnScreenText("DOOR OPENS");
-					
-				} else {
-					// collision.animation.Play ("Door|DoorAction");
-					// in reverse
-					SetOnScreenText("FAILED! NO POWER ON DOOR");
+				if (onScreenText != "" && CurrentOnScreenText == onScreenText) {
+						yield return new WaitForSeconds (5);
+						onScreenText = "";
 				}
-			}
 		}
 		
-		if (Collider != null && Collider.gameObject.tag == "Collectable")
+		// Place here all "enter" events like pop-up messages
+		void OnTriggerEnter (Collider collision)
 		{
-			if (Input.GetKeyDown(KeyCode.Space)) {
-				SetOnScreenText("Successfully collected the " + Collider.gameObject.name);
-				AudioSource.PlayClipAtPoint(collectSound, transform.position);
-				CollectedGameObjects.Add(Collider.gameObject);
-				Collider.gameObject.SetActive(false);
-				Destroy(Collider.gameObject.collider);
+				Collider = collision;
+				if (Collider.gameObject.tag == "Switch") {
+						SetOnScreenText ("This is a Power Switch. To toggle the switch press the spacebar");
+				}
+		
+				if (Collider.gameObject.tag == "Door") {
+						SetOnScreenText ("To open the door press the spacebar");
+				}
+		
+				if (Collider.gameObject.tag == "Refrigerator") {
+						if (!PlayedGameObjects.Contains (Collider.gameObject)) {
+								Collider.audio.Play ();
+								Collider.animation.Play ();
+								PlayedGameObjects.Add (Collider.gameObject);
+						}
+				}
+		
+				if (Collider.gameObject.tag == "CollectableConsumable" || Collider.gameObject.tag == "CollectableReusable") {
+						SetOnScreenText ("To collect the " + Collider.gameObject.name + " press the spacebar!");
+				}
+		}
+		
+		// Place here al "exit" events like removing the pop-up messages
+		void OnTriggerExit (Collider collision)
+		{
+				SetOnScreenText ("");
 				Collider = null;
-			}
-		}
-	}
-	
-	void SetOnScreenText(string text) {
-		onScreenText = text;
-		StartCoroutine(AutoRemoveOnScreenText(onScreenText));
-	}
-	
-	IEnumerator AutoRemoveOnScreenText(string CurrentOnScreenText) {
-		if (onScreenText != "" && CurrentOnScreenText == onScreenText)
-		{
-			yield return new WaitForSeconds(5);
-			onScreenText = "";
-		}
-	}
-	
-	// Place here all "enter" events like pop-up messages
-	void OnTriggerEnter(Collider collision)
-	{
-		Collider = collision;
-		if (Collider.gameObject.tag == "Switch")
-		{
-			SetOnScreenText("This is a Power Switch. To toggle the switch press the spacebar");
 		}
 		
-		if (Collider.gameObject.tag == "Door")
+		void OnGUI ()
 		{
-			SetOnScreenText("To open the door press the spacebar");
-		}
+				GUI.skin.font = Font;
 		
-		if (Collider.gameObject.tag == "Refrigerator") 
-		{
-			if (!PlayedGameObjects.Contains(Collider.gameObject))
-			{
-				Collider.audio.Play();
-				Collider.animation.Play ();
-				PlayedGameObjects.Add(Collider.gameObject);
-			}
-		}
-		
-		if (Collider.gameObject.tag == "Collectable") 
-		{
-			SetOnScreenText("To collect the " + Collider.gameObject.name + " press the spacebar!");
-		}
-	}
-	
-	// Place here al "exit" events like removing the pop-up messages
-	void OnTriggerExit(Collider collision)
-	{
-		SetOnScreenText("");
-		Collider = null;
-	}
-	
-	void OnGUI() {
-		GUI.skin.font = Font;
-		
-		if (onScreenText.Length > 0)
-		{
-			string[] lines = onScreenText.Split('\n');
-			int longestLineLength = 0;
-			for (int i = 0; i < lines.Length; i++)
-			{
-				if (lines[i].Length > longestLineLength)
-				{
-					longestLineLength = lines[i].Length;
+				if (onScreenText.Length > 0) {
+						string[] lines = onScreenText.Split ('\n');
+						int longestLineLength = 0;
+						for (int i = 0; i < lines.Length; i++) {
+								if (lines [i].Length > longestLineLength) {
+										longestLineLength = lines [i].Length;
+								}
+								i++;
+						}
+						int width = longestLineLength * 9 + 30;
+						int height = lines.Length * 16 + 20;
+						int x = Screen.width / 2 - width / 2;
+						int y = Screen.height / 2 - height / 2;
+						GUI.Box (new Rect (x, y, width, height), onScreenText);
 				}
-				i++;
-			}
-			int width = longestLineLength*9 + 30;
-			int height = lines.Length*16 + 20;
-			int x = Screen.width/2 - width/2;
-			int y = Screen.height/2 - height/2;
-			GUI.Box(new Rect(x, y, width, height), onScreenText);
-		}
-
-		if (showCollectables) {
-			GUI.Box (new Rect (10,10,250,CollectedGameObjects.Count*30 + 30), "Collected items: " + CollectedGameObjects.Count);
-			for (int i = 0; i < CollectedGameObjects.Count; i++)
-			{
-				if (GUI.Button(new Rect(20,i*30 + 40,80,20), CollectedGameObjects[i].name)) {
-					CollectedGameObjects[i].SetActive(true);
-					CollectedGameObjects[i].transform.position = transform.position;
-					CollectedGameObjects[i].transform.Translate(0.4f, 0.15f, 0.75f);
-					CollectedGameObjects[i].transform.rotation = transform.rotation;
-					CollectedGameObjects[i].transform.Rotate(0, 90, 0);
-					CollectedGameObjects[i].transform.parent = transform;
-				}
-			}
-		}
-
 		
-	}
+				if (showCollectables) {
+						GUI.Box (new Rect (10, 10, 250, CollectedGameObjects.Count * 30 + 30), "Collected items: " + CollectedGameObjects.Count);
+						for (int i = 0; i < CollectedGameObjects.Count; i++) {
+								if (GUI.Button (new Rect (20, i * 30 + 40, 80, 20), CollectedGameObjects [i].name)) {
+										if (CollectedGameObjects [i].tag == "CollectableReusable") {
+												Reuse (CollectedGameObjects [i]);
+										}
+										if (CollectedGameObjects [i].tag == "CollectableConsumable") {
+												Consume (CollectedGameObjects [i]);
+										}
+								}
+						}
+				}
+		}
+		
+		void Reuse (GameObject gameobject)
+		{
+				if (!gameobject.activeSelf) {
+						gameobject.SetActive (true);
+						gameobject.transform.position = transform.position;
+						gameobject.transform.Translate (0.4f, 0.15f, 0.75f);
+						gameobject.transform.rotation = transform.rotation;
+						gameobject.transform.Rotate (0, 90, 0);
+						gameobject.transform.parent = transform;
+				} else {
+						gameobject.SetActive (false);
+				}
+		}
+		
+		void Consume (GameObject gameobject)
+		{
+				CollectedGameObjects.Remove (gameobject);
+				Destroy (gameobject);
+				SetOnScreenText ("Wat een held ben je ook! Je hebt zojuist " + gameobject.name + " in je ...... gestoken");
+		}
 }
