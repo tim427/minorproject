ece18b8fe6b04ca1081d15a0d62c46aa8ddd754d
2ArounD <arnoudjonker@gmail.com>
2014-12-16 12:41:58 +0100

    navmesh gecombineerd met enemycontroller

--- Unity_Projects/BasisLevelBeta/Assets/scripts/NavMeshPathfinder.cs
+++ Unity_Projects/BasisLevelBeta/Assets/scripts/NavMeshPathfinder.cs
@@ -3,17 +3,64 @@ using System.Collections;
 
 public class NavMeshPathfinder : MonoBehaviour {
 
-	private NavMeshAgent navMesh;
+
 	public Transform target;
+	private NavMeshAgent navMesh;
+	private bool isTriggered;
+	private bool turningAroundNow;
+	private bool moveBackNow;
+	private Vector3 initialPos;
+
 
 	// Use this for initialization
 	void Start () {
-	
+
+		initialPos = transform.position;
+		isTriggered = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		navMesh = GetComponent<NavMeshAgent>();
-		navMesh.SetDestination(target.position);
+
+
+		Vector3 targetDir = target.transform.position - transform.position;
+		Vector3 forward = transform.forward;
+		float angle = Vector3.Angle(forward, targetDir);
+		float distance = Vector3.Distance(forward, targetDir);
+		moveBackNow = true;
+
+		// checking if guard detects player
+		if (angle < 25.0F && distance < 3.5F && !isTriggered)
+		{
+			// when detected set destination
+			print ("Detected by " + name);
+			turningAroundNow = false;
+			moveBackNow = false;
+			isTriggered = true;
+		}
+
+
+		if (isTriggered) {
+			navMesh = GetComponent<NavMeshAgent>();
+			navMesh.SetDestination(target.position);
+			isTriggered = false;
+
+		}
+		if (moveBackNow) {	
+			print ("Moving back");
+			navMesh = GetComponent<NavMeshAgent>();
+			navMesh.SetDestination(initialPos);
+			turningAroundNow = true;
+			moveBackNow = false;
+		}
+		if (turningAroundNow) {
+		}
+
+
+	}
+
+	public void triggerGuard() {
+
 	}
 }
