2b49dfa190d03616c41ad2d6b5261e4ad440c0e3
2ArounD <arnoudjonker@gmail.com>
2014-12-18 10:07:49 +0100

    progress on enemy navmesh business

--- Unity_Projects/BasisLevelBeta/Assets/scripts/NavMeshPathfinder.cs
+++ Unity_Projects/BasisLevelBeta/Assets/scripts/NavMeshPathfinder.cs
@@ -4,17 +4,28 @@ using System.Collections;
 public class NavMeshPathfinder : MonoBehaviour {
 
 
+
+	public float searchAngle;
+	public float detectionAngle;
+	public float detectionDistance;
 	public Transform target;
+	public int turnspeed = 1;
+
+	private Vector3 initialRot;
 	private NavMeshAgent navMesh;
 	private bool isTriggered;
 	private bool turningAroundNow;
 	private bool moveBackNow;
 	private Vector3 initialPos;
+	private Vector3 targetRot;
+	private int factor = -1;
 
 
 	// Use this for initialization
 	void Start () {
 
+
+		initialRot = transform.forward;
 		initialPos = transform.position;
 		isTriggered = false;
 
@@ -24,43 +35,78 @@ public class NavMeshPathfinder : MonoBehaviour {
 	void Update () {
 
 
+//		transform.Rotate(Time.deltaTime, 0, 0);
+
 		Vector3 targetDir = target.transform.position - transform.position;
 		Vector3 forward = transform.forward;
 		float angle = Vector3.Angle(forward, targetDir);
 		float distance = Vector3.Distance(forward, targetDir);
-		moveBackNow = true;
+		isTriggered = false;
 
 		// checking if guard detects player
-		if (angle < 25.0F && distance < 3.5F && !isTriggered)
+		if (angle < detectionAngle && distance < detectionDistance && !isTriggered)
 		{
 			// when detected set destination
-			print ("Detected by " + name);
-			turningAroundNow = false;
-			moveBackNow = false;
+			navMesh = GetComponent<NavMeshAgent>();
+			navMesh.SetDestination(target.position);
 			isTriggered = true;
 		}
 
 
-		if (isTriggered) {
-			navMesh = GetComponent<NavMeshAgent>();
-			navMesh.SetDestination(target.position);
-			isTriggered = false;
-
-		}
-		if (moveBackNow) {	
-			print ("Moving back");
+		if (!isTriggered) {
 			navMesh = GetComponent<NavMeshAgent>();
 			navMesh.SetDestination(initialPos);
-			turningAroundNow = true;
-			moveBackNow = false;
-		}
-		if (turningAroundNow) {
+
 		}
 
+		bool rotating = true;
 
+	
+		if (rotating = true){
+
+			float step = turnspeed * Time.deltaTime;
+
+			print (transform.forward == targetRot);
+			print (transform.forward);
+			print (targetRot);
+
+
+			if (transform.forward==initialRot){
+				print ("check");
+				factor = factor*-1;
+				targetRot = new Vector3(Mathf.Sin ( Mathf.Deg2Rad * factor *searchAngle),0,Mathf.Cos(Mathf.Deg2Rad * factor * searchAngle));
+			}
+
+			else if (transform.forward == targetRot){
+				print ("check1");
+				targetRot = initialRot;
+			}
+
+			Vector3 direction = Vector3.RotateTowards (transform.forward, targetRot, step, 0.0F);
+
+			transform.rotation = Quaternion.LookRotation (direction);
+
+		}
+
+
+//		if (navMesh.destination == transform.position)
+//
+//
+//		if (moveBackNow) {	
+//			print ("Moving back");
+//			navMesh = GetComponent<NavMeshAgent>();
+//			navMesh.SetDestination(initialPos);
+//			turningAroundNow = true;
+//			moveBackNow = false;
+//		}
+//		if (turningAroundNow) {
+//
+//			float step = turnspeed * Time.deltaTime;
+//			targetDir = initialPos - transform.position;
+//			Vector3 direction = Vector3.RotateTowards (transform.forward, targetDir, step, 0.0F);
+//			transform.rotation = Quaternion.LookRotation (direction);
+//
+//		}
 	}
 
-	public void triggerGuard() {
-
-	}
 }
