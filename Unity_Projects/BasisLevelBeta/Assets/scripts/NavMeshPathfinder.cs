31257d2855b9760b177f38b2c0b35e90e1f59d63
Tim de Boer <tim@tim427.net>
2014-12-19 13:39:02 +0100

    CameraController link to Drone :fearful:

--- Unity_Projects/BasisLevelBeta/Assets/scripts/NavMeshPathfinder.cs
+++ Unity_Projects/BasisLevelBeta/Assets/scripts/NavMeshPathfinder.cs
@@ -3,226 +3,227 @@ using System.Collections;
 
 [RequireComponent(typeof(NavMeshAgent))]
 
-public class NavMeshPathfinder : MonoBehaviour {
+public class NavMeshPathfinder : MonoBehaviour
+{
 
 
-	public Light stateLight;
-	public float searchAngle;
-	public float detectionAngle;
-	public float detectionDistance;
-	public Transform target;
-	public float turnspeed = 1;
+		public Light stateLight;
+		public float searchAngle;
+		public float detectionAngle;
+		public float detectionDistance;
+		public Transform target;
+		public float turnspeed = 1;
+		public bool linkedWithCamera = true;
+		private float timeSinceDetection;
+		private bool detection;
+		private float startState2;
+		private float timeSinceMovingInState4;
+		private int state;
+		private Vector3 initialRot;
+		private NavMeshAgent navMesh;
+		private Vector3 initialPos;
+		private Vector3 targetRot;
+		private int factor = -1;
+		private float initialAngle;
+		private Vector3 targetPos;
 
-	private float timeSinceDetection;
-	private bool detection;
-	private float startState2;
-	private float timeSinceMovingInState4;
-	private int state;
-	private Vector3 initialRot;
-	private NavMeshAgent navMesh;
-	private Vector3 initialPos;
-	private Vector3 targetRot;
-	private int factor = -1;
-	private float initialAngle;
-	private Vector3 targetPos;
 
-
-	// Use this for initialization
-	void Start () {
-
-		detection = false;
-		state = 0;
-
-		initialRot = transform.forward;
-		targetRot = initialRot;
-		initialAngle = Vector3.Angle( Vector3.forward, initialRot);
-		initialPos = transform.position;
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-		print ("state" + state);
-
-		Vector3 targetDir = target.transform.position - transform.position;
-		Vector3 forward = transform.forward;
-		float angle = Vector3.Angle(forward, targetDir);
-		float distance = Vector3.Distance(forward, targetDir);
-
-		if (!detection){
-			timeSinceDetection = Time.time;
-		}
-		detection = false;
-
-		// checking if guard detects player
-		if (angle < detectionAngle && distance < detectionDistance)
+		// Use this for initialization
+		void Start ()
 		{
-			if ( Time.time-timeSinceDetection>2){
 
-				state = 3;
-			}
+				detection = false;
+				state = 0;
 
-			else if (state == 2 || state == 4){
+				initialRot = transform.forward;
+				targetRot = initialRot;
+				initialAngle = Vector3.Angle (Vector3.forward, initialRot);
+				initialPos = transform.position;
 
-				state = 3;
-
-			}
-
-			else  if ( state < 3){
-				state = 1;
-			}
-
-			detection = true;
-
-			targetPos = target.position;
 		}
 	
-		else if( state == 4 && Time.time-timeSinceMovingInState4 > 10){
-			state = 2;
-			startState2 = Time.time;
+		// Update is called once per frame
+		void Update ()
+		{
+
+				print ("state" + state);
+
+				Vector3 targetDir = target.transform.position - transform.position;
+				Vector3 forward = transform.forward;
+				float angle = Vector3.Angle (forward, targetDir);
+				float distance = Vector3.Distance (forward, targetDir);
+
+				if (!detection) {
+						timeSinceDetection = Time.time;
+				}
+				detection = false;
+
+				// checking if guard detects player
+				if (angle < detectionAngle && distance < detectionDistance) {
+						if (Time.time - timeSinceDetection > 2) {
+
+								state = 3;
+						} else if (state == 2 || state == 4) {
+
+								state = 3;
+
+						} else  if (state < 3) {
+								state = 1;
+						}
+
+						detection = true;
+
+						targetPos = target.position;
+				}
+		
+				if (state != 3 && CameraController.x != 0) {
+						state = 5;
+				} else if (state == 4 && Time.time - timeSinceMovingInState4 > 10) {
+						state = 2;
+						startState2 = Time.time;
+				} else if (state == 3) {
+						state = 4;
+
+				} else if (state == 2 && Time.time - startState2 > 10) {
+						state = 0;
+				} else if (state == 1) {
+						state = 0;
+				} else {
+						detection = false;
+				}
+
+
+
+
+				switch (state) {
+				case 0:
+
+						MoveTo (initialPos);
+
+						TurnTo (initialRot);
+
+						LedColour ("green");
+
+						break;
+
+				case 1:
+
+						TurnTo (targetPos - transform.position);
+
+						LedColour ("yellow");
+
+						break;
+
+				case 2:
+						MoveTo (initialPos);
+
+						LookAround (searchAngle);
+
+						LedColour ("yellow");
+
+						break;
+
+				case 3: 
+
+						MoveTo (targetPos);
+
+						LedColour ("red");
+
+						break;
+
+				case 4:
+
+						LedColour ("yellow");
+
+
+						if (targetPos.x == transform.position.x && targetPos.z == targetPos.z) {
+
+								print ("targetPos bereiekt");
+
+								LookAround (170);
+
+						} else {
+
+								timeSinceMovingInState4 = Time.time;
+								MoveTo (targetPos);
+						}
+
+						break;
+
+				case 5: 
+						Vector3 cameraLastSeenPos = new Vector3 (CameraController.x, transform.position.y, CameraController.z);
+			
+						MoveTo (cameraLastSeenPos);
+			
+						LedColour ("yellow");
+			
+						break;
+				}
+
+
+
 		}
 
-		else if ( state == 3){
-			state = 4;
+		// method to make enemy look arnoud with variable searchangle
+		public void LookAround (float angle)
+		{
+				float step = turnspeed * Time.deltaTime;
 
-		}
-
-		else if (state == 2 && Time.time-startState2 > 10){
-			state = 0;
-		}
-
-		else if (state == 1 ){
-			state = 0;
-		}
-
-		else {
-			detection = false;
-		}
-
-
-
-
-		switch (state){
-		case 0 :
-
-			MoveTo(initialPos);
-
-			TurnTo (initialRot);
-
-			LedColour("green");
-
-			break;
-
-		case 1 :
-
-			TurnTo (targetPos-transform.position);
-
-			LedColour("yellow");
-
-			break;
-
-		case 2 :
-			MoveTo (initialPos);
-
-			LookAround(searchAngle);
-
-			LedColour("yellow");
-
-			break;
-
-		case 3 : 
-
-			MoveTo (targetPos);
-
-			LedColour("red");
-
-			break;
-
-		case 4 :
-
-			LedColour("yellow");
-
-
-			if (targetPos.x==transform.position.x && targetPos.z == targetPos.z ){
-
-				print ("targetPos bereiekt");
-
-				LookAround(170);
-
-			}
-
-			else {
-
-				timeSinceMovingInState4 = Time.time;
-				MoveTo (targetPos);
-			}
-
-			break;
-		}
-
-
-
-	}
-
-	// method to make enemy look arnoud with variable searchangle
-	public void LookAround(float angle) {
-		float step = turnspeed * Time.deltaTime;
-
-		if (Vector3.Distance(transform.forward, targetRot)<0.01F){
-			transform.forward = targetRot;
-		}
+				if (Vector3.Distance (transform.forward, targetRot) < 0.01F) {
+						transform.forward = targetRot;
+				}
 		
 		
-		if (transform.forward == initialRot){
-			factor = factor*-1;
-			targetRot = new Vector3(Mathf.Sin ( Mathf.Deg2Rad * factor *(angle+initialAngle*factor)),0,Mathf.Cos(Mathf.Deg2Rad * factor * (angle+initialAngle*factor)));
-		}
+				if (transform.forward == initialRot) {
+						factor = factor * -1;
+						targetRot = new Vector3 (Mathf.Sin (Mathf.Deg2Rad * factor * (angle + initialAngle * factor)), 0, Mathf.Cos (Mathf.Deg2Rad * factor * (angle + initialAngle * factor)));
+				} else if (transform.forward == targetRot) {
+						targetRot = initialRot;
+				}
 		
-		else if (transform.forward == targetRot){
-			targetRot = initialRot;
-		}
+				Vector3 direction = Vector3.RotateTowards (transform.forward, targetRot, step, 0.0F);
 		
-		Vector3 direction = Vector3.RotateTowards (transform.forward, targetRot, step, 0.0F);
-		
-		transform.rotation = Quaternion.LookRotation (direction);
+				transform.rotation = Quaternion.LookRotation (direction);
 
-	}
-
-	public void MoveTo(Vector3 position){
-
-		navMesh = GetComponent<NavMeshAgent>();
-		navMesh.SetDestination(position);
-
-		if (Vector3.Distance(transform.position, position) < 0.01f) {
-			transform.position = position;
-		}
-	}
-
-	public void TurnTo(Vector3 orientation){
-
-		float step = turnspeed * Time.deltaTime;
-		Vector3 direction = Vector3.RotateTowards (transform.forward, orientation, step, 0.0F);
-		transform.rotation = Quaternion.LookRotation (direction);
-	}
-
-	public void LedColour(string colour){
-
-		switch(colour){
-
-		case "red":
-			stateLight.color = Color.red;
-			break;
-
-		case "green":
-			stateLight.color = Color.green;
-			break;
-
-		case "yellow":
-			stateLight.color = Color.yellow;
-			break;
 		}
 
-	}
+		public void MoveTo (Vector3 position)
+		{
+
+				navMesh = GetComponent<NavMeshAgent> ();
+				navMesh.SetDestination (position);
+
+				if (Vector3.Distance (transform.position, position) < 0.01f) {
+						transform.position = position;
+				}
+		}
+
+		public void TurnTo (Vector3 orientation)
+		{
+
+				float step = turnspeed * Time.deltaTime;
+				Vector3 direction = Vector3.RotateTowards (transform.forward, orientation, step, 0.0F);
+				transform.rotation = Quaternion.LookRotation (direction);
+		}
+
+		public void LedColour (string colour)
+		{
+
+				switch (colour) {
+
+				case "red":
+						stateLight.color = Color.red;
+						break;
+
+				case "green":
+						stateLight.color = Color.green;
+						break;
+
+				case "yellow":
+						stateLight.color = Color.yellow;
+						break;
+				}
+
+		}
 
 }
