08f1ec3fd80dfdd141828e7429f6f5a02e0cf80e
Jordi1080 <jordi.hesselz@hotmail.com>
2015-01-12 17:55:02 +0100

    gui start

---
+++ Unity_Projects/BasisLevelBeta/Assets/scripts/Menu/GameStarter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStarter : MonoBehaviour {
+
+	// Use this for initialization
+	void OnMouseDown () {
+		print ("mouse click");
+		Application.LoadLevel ("Hoofdscene");
+	}
+
+}
