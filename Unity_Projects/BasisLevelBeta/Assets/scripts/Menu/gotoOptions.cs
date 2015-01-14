32cd99e81cd57ad56b047944790cee4b94d90276
Jordi1080 <jordi.hesselz@hotmail.com>
2015-01-14 22:11:39 +0100

    logo voor de app en titel met 3d letters

--- Unity_Projects/BasisLevelBeta/Assets/scripts/Menu/gotoOptions.cs
+++ Unity_Projects/BasisLevelBeta/Assets/scripts/Menu/gotoOptions.cs
@@ -1,33 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/*
+
 public class gotoOptions : MonoBehaviour {
-
+	public bool menu;
 	public bool options;
-
-	// Use this for initialization
+	private bool video;
+	private bool audiosettings;
+	public float fieldOfView = 60f;
+	public float sfxVolume = 0.5f;
+	public float musicVolume = 0.5f;
 	
-	// Update is called once per frame
+	public int resX;
+	public int resY;
+
 	void OnMouseClick () {
 		print ("clicked on options");
-		optionsMenu ();
+		GameObject obj = GameObject.Find ("First Person Controller");
+		MainMenu menu = obj.GetComponent<MainMenu> ();
+		menu.optionsMenu ();
+		}
 
-	private void optionsMenu {
-		if (GUI.Button (new Rect (Screen.width / 2 - 75, Screen.height / 2 - 50, 150, 30), "Video Settings")) {
-			options = false;
-			video = true;
-		}
+	public void optionsMenu() {
+				if (GUI.Button (new Rect (Screen.width / 2 - 75, Screen.height / 2 - 50, 150, 30), "Video Settings")) {
+						options = false;
+						video = true;
+				}
 		
-		if (GUI.Button (new Rect (Screen.width / 2 - 75, Screen.height / 2 - 0, 150, 30), "Audio Settings")) {
-			options = false;
-			audiosettings = true;
-		}
+				if (GUI.Button (new Rect (Screen.width / 2 - 75, Screen.height / 2 - 0, 150, 30), "Audio Settings")) {
+						options = false;
+						audiosettings = true;
+				}
 		
-		if (GUI.Button (new Rect (Screen.width / 2 - 75, Screen.height / 2 + 50, 150, 30), "Back")) {
-			menu = true;
-			options = false;
+				if (GUI.Button (new Rect (Screen.width / 2 - 75, Screen.height / 2 + 50, 150, 30), "Back")) {
+						menu = true;
+						options = false;
+				}
 		}
-	}
 }
-*/
